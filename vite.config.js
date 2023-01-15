1f21df8a5d986cc81d72926e37b1f9d5813073b9
Julio Negri <julioyuki@outlook.com>
2023-01-15 20:49:49 -0300

    feat: adding vite build for the lub

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/Tooltip.jsx',
+      fileName: 'Tooltip',
+      name: 'Tulitip',
+      formats: ['es', 'cjs']
+    }
+  }
+})
