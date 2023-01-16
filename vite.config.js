e22c7b07b6970599145debb7c456071db7f91c04
Julio Negri <julioyuki@outlook.com>
2023-01-15 21:03:43 -0300

    fix: fixing lib build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import * as packageJson from './package.json';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
       entry: 'src/Tooltip.jsx',
-      fileName: 'Tooltip',
+      fileName: (format) => `tulitip.${format}.js`,
       name: 'Tulitip',
-      formats: ['es', 'cjs']
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)]
     }
   }
 })
